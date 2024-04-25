288777c10dfd9d3d94218cbe3edbef40d70b9696
EduardoVarela0144 <eduardo.varela142001@gmail.com>
2024-04-24 18:25:38 -0600

    controller restaurant

--- app/Http/Controllers/RestauranteController.php
+++ app/Http/Controllers/RestauranteController.php
@@ -8,6 +8,32 @@ use Illuminate\Http\Response;
 
 class RestauranteController extends Controller
 {
+    public function index()
+    {
+        $restaurantes = Restaurante::all();
+
+        return response()->json([
+            'message' => 'Lista de restaurantes obtenida exitosamente',
+            'restaurantes' => $restaurantes,
+        ], Response::HTTP_OK);
+    }
+
+    public function show($id)
+    {
+        $restaurante = Restaurante::find($id);
+
+        if (!$restaurante) {
+            return response()->json([
+                'message' => 'Restaurante no encontrado',
+            ], Response::HTTP_NOT_FOUND);
+        }
+
+        return response()->json([
+            'message' => 'Restaurante obtenido exitosamente',
+            'restaurante' => $restaurante,
+        ], Response::HTTP_OK);
+    }
+
     public function store(Request $request)
     {
         $validatedData = $request->validate([
@@ -25,5 +51,49 @@ class RestauranteController extends Controller
             'message' => 'Restaurante creado exitosamente',
             'restaurante' => $restaurante,
         ], Response::HTTP_CREATED);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $validatedData = $request->validate([
+            'name' => 'required|string',
+            'address' => 'required|string',
+            'phone' => 'required|string',
+            'type' => 'required|string',
+            'description' => 'required|string',
+            'image' => 'required|string',
+        ]);
+
+        $restaurante = Restaurante::find($id);
+
+        if (!$restaurante) {
+            return response()->json([
+                'message' => 'Restaurante no encontrado',
+            ], Response::HTTP_NOT_FOUND);
+        }
+
+        $restaurante->update($validatedData);
+
+        return response()->json([
+            'message' => 'Restaurante actualizado exitosamente',
+            'restaurante' => $restaurante,
+        ], Response::HTTP_OK);
+    }
+
+    public function destroy($id)
+    {
+        $restaurante = Restaurante::find($id);
+
+        if (!$restaurante) {
+            return response()->json([
+                'message' => 'Restaurante no encontrado',
+            ], Response::HTTP_NOT_FOUND);
+        }
+
+        $restaurante->delete();
+
+        return response()->json([
+            'message' => 'Restaurante eliminado exitosamente',
+        ], Response::HTTP_OK);
     }
 }
